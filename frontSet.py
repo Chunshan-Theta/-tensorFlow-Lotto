36c3d2a5ea748cc56bdd378ef7bb9611736371fd
Wang <gavin19950511@gmail.com>
2017-06-28 01:26:24 +0800

    fin
    close

--- frontSet.py
+++ frontSet.py
@@ -1,6 +1,7 @@
 import csv
 ArrayAward = []
-ArrayAnsFactor=[]
+ArrayAns = []
+ArrayFoctor=[]
 with open('source.csv', 'rb') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
     for row in spamreader:
@@ -12,13 +13,22 @@ for index in range(4):
     for i in range(6):    
         ArraySample=[]
         for j in range(10):
-            for k in range(6):            
-                ArraySample.append(int(ArrayAward[j+index][2+k]))
+	    ArraySample_resource=[]
+            for k in range(6):    
+		 ArraySample_resource.append(int(ArrayAward[j+index][2+k]))       
+            ArraySample.append(ArraySample_resource)
         #ArraySample.sort()
         #print ArrayAward[index+10][i+2],ArraySample
         #print index,"*"*50
-        ArrayAnsFactor.append([int(ArrayAward[index+10][i+2]),ArraySample])
-#print ArrayAnsFactor[1]
-import json
-JsonArray = json.dumps(ArrayAnsFactor)
-print JsonArray
+        #print int(ArrayAward[index+10][i+2]),ArraySample
+	ArrayFoctor.append(ArraySample)
+	ans=int(ArrayAward[index+10][i+2])-1
+	ArrayAns_resource=[]
+	for Ln in range(48):
+	    if Ln!=ans:
+	        ArrayAns_resource.append(0)
+	    else:
+		ArrayAns_resource.append(1)
+	ArrayAns.append(ArrayAns_resource)	
+print ArrayFoctor[0]
+print ArrayAns[0]
